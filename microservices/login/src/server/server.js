62cfa0ece5e1e4e72e0eb5fb15348434327fcf13
ronal-triana-adl <ronal.triana@avaldigitallabs.com>
2019-03-02 12:51:30 -0500

    Se habilita el urlencode en el micro de login

--- microservices/login/src/server/server.js
+++ microservices/login/src/server/server.js
@@ -16,7 +16,7 @@ const start = (options) => {
 
         const app = express()
         // parse application/x-www-form-urlencoded
-        app.use(bodyParser.urlencoded({ extended: false }))
+        app.use(bodyParser.urlencoded({ extended: true }))
 
         // parse application/json
         app.use(bodyParser.json())
