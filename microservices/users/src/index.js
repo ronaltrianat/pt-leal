36f45c25351a943ac5b08b625689d32f900b24a2
ronal-triana-adl <ronal.triana@avaldigitallabs.com>
2019-03-03 10:52:45 -0500

    Se eliminan los then encadenados y se implementa await

--- microservices/users/src/index.js
+++ microservices/users/src/index.js
@@ -4,12 +4,25 @@ const server = require('./server/server')
 const config = require('./config/')
 const repository = require('./repository/repository')
 
-config.db.connect(config.dbSettings)
-    .then(poolDB => repository.connect(poolDB))
-    .then(repo => server.start({ port: config.serverSettings.port, repo}))
-    .then(app => {
-        console.log(`Server started succesfully, running on port: ${config.serverSettings.port}.`)
-        app.on('close', () => {
-          console.log('close disconect');
-        })
-    });
+const start = async () => {
+  try {
+    let poolDB = await config.db.connect(config.dbSettings)
+   
+    let repo = await repository.connect(poolDB)
+    
+    let app = await server.start({ port: config.serverSettings.port, repo})
+
+    console.log(`Server started succesfully, running on port: ${config.serverSettings.port}.`)
+
+    app.on('close', () => {
+      console.log('close disconect')
+    })
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+/** Iniciar Microservicio **/
+start()
+
+
